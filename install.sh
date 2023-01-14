111a97c4d89cd6f04064d83fb15b832ee5b7959d
NU-LL <1125934312@qq.com>
2023-01-15 03:25:45 +0800

    FIX: add some vim config and detach vim configuration file

--- install.sh
+++ install.sh
@@ -152,10 +152,12 @@ display_log "Start installing dependent software"
 interactive_config_system_type
 case "${SYSTEM_TYPE}" in
 	"archlinux")
-        sudo pacman -Syu git vim zsh curl wget dialog coreutils python-pygments
+        sudo pacman -Syu git vim zsh curl wget dialog coreutils python-pygments nodejs npm
         ;;
 	"ubuntu")
         sudo apt install git vim zsh curl wget dialog stty command-not-found python3-pygments
+        # vim coc 插件需要最新版 nodejs 支持，ubuntu 默认仓库中的版本较低
+        # curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - && sudo apt install nodejs
         ;;
 	*)
 		echo "unknown os: ${SYSTEM_TYPE}"
@@ -274,31 +276,7 @@ if [ -f ${TARGET_PATH}/.vimrc ];then
 fi
 
 
-cat > ${TARGET_PATH}/.vimrc <<EOF
-set number " 显示行号
-set showcmd " 显示完整命令
-set relativenumber " 显示相对行号（这个非常重要，慢慢体会）
-set hlsearch " 搜索结果高亮
-set autoindent " 自动缩进
-set smartindent " 智能缩进
-set tabstop=4 " 设置 tab 制表符所占宽度为 4
-set softtabstop=4 " 设置按 tab 时缩进的宽度为 4
-set shiftwidth=4 " 设置自动缩进宽度为 4
-set expandtab " 缩进时将 tab 制表符转换为空格
-set mouse= " 关闭鼠标功能
-"set mouse=a " 启用全部鼠标功能
-set wildmenu " 启用选项菜单
-set cursorline " 启用下划线
-
-set fileencodings=utf8,cp936,gb18030,big5 " 尝试按照这些编码方式逐一探测
-filetype on " 开启文件类型检测
-syntax on " 开启语法高亮
-" 记住上次编辑的位置
-au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
-EOF
-
-
-
+cat ./vimrc.template > ${TARGET_PATH}/.vimrc 
 
 
 interactive_config_install_vim_plugs
@@ -311,74 +289,5 @@ if [ x"${INSTALL_VIM_PLUGS}" == x"yes" ];then
     fi
 
     # set .vimrc
-    cat >> ${TARGET_PATH}/.vimrc <<EOF
-
-
-" ==================== VIM PLUGS ====================
-call plug#begin()
-
-    Plug 'yianwillis/vimcdoc'
-    Plug 'preservim/nerdtree'
-    Plug 'preservim/nerdcommenter'
-    Plug 'vim-airline/vim-airline'
-    Plug 'vim-airline/vim-airline-themes'
-
-call plug#end()
-
-
-" NERDTree Config
-nnoremap <leader>n :NERDTreeFocus<CR>
-nnoremap <C-n> :NERDTree<CR>
-nnoremap <C-t> :NERDTreeToggle<CR>
-nnoremap <C-f> :NERDTreeFind<CR>
-
-
-" nerdcommenter Config
-" Add spaces after comment delimiters by default
-let g:NERDSpaceDelims = 1
-" Use compact syntax for prettified multi-line comments
-let g:NERDCompactSexyComs = 1
-" Align line-wise comment delimiters flush left instead of following code indentation
-let g:NERDDefaultAlign = 'left'
-" Set a language to use its alternate delimiters by default
-let g:NERDAltDelims_java = 1
-" Add your own custom formats or override the defaults
-let g:NERDCustomDelimiters = { 'c': { 'left': '/**','right': '*/' } }
-" Allow commenting and inverting empty lines (useful when commenting a region)
-let g:NERDCommentEmptyLines = 1
-" Enable trimming of trailing whitespace when uncommenting
-let g:NERDTrimTrailingWhitespace = 1
-" Enable NERDCommenterToggle to check all selected lines is commented or not
-let g:NERDToggleCheckAllLines = 1
-" \cc 注释当前行和选中行
-" \cn 没有发现和\cc有区别
-" \c<空格> 如果被选区域有部分被注释，则对被选区域执行取消注释操作，其它情况执行反转注释操作
-" \cm 对被选区域用一对注释符进行注释，前面的注释对每一行都会添加注释
-" \ci 执行反转注释操作，选中区域注释部分取消注释，非注释部分添加注释
-" \cs 添加性感的注释，代码开头介绍部分通常使用该注释
-" \cy 添加注释，并复制被添加注释的部分
-" \c$ 注释当前光标到改行结尾的内容
-" \cA 跳转到该行结尾添加注释，并进入编辑模式
-" \ca 转换注释的方式，比如：/**/和//
-" \cl \cb 左对齐和左右对其，左右对其主要针对/**/
-" \cu 取消注释
-
-
-" vim-airline Config
-set laststatus=2  "永远显示状态栏
-let g:airline_powerline_fonts = 1  " 支持 powerline 字体
-let g:airline#extensions#tabline#enabled = 1 " 显示窗口tab和buffer
-let g:airline_theme='murmur'  " murmur配色不错
-if !exists('g:airline_symbols')
-    let g:airline_symbols = {}
-endif
-let g:airline_left_sep = '▶'
-let g:airline_left_alt_sep = '❯'
-let g:airline_right_sep = '◀'
-let g:airline_right_alt_sep = '❮'
-let g:airline_symbols.linenr = '¶'
-let g:airline_symbols.branch = '⎇'
-
-EOF
-
+    cat ./vimrc.plugs.template >> ${TARGET_PATH}/.vimrc 
 fi
